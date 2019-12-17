d65147194cdb98885eddea1c5ccb2493e08e4a43
Xiya-Duan <x.duan.1@student.rug.nl>
2019-12-17 16:53:22 +0100

    Add comments

--- Lab4/Assignment5/assignment5.m
+++ Lab4/Assignment5/assignment5.m
@@ -5,11 +5,15 @@ m = length(x);       % original sample length
 n = pow2(nextpow2(m));  % transform length
 y = fft(x,n);        % DFT of signal
 f = (0:n-1)*(fs/n);
-power = abs(y).^2/n;      
-[b,a] = butter(4, [150,700]/(fs/2));
-output = filter(b,a,x);
-audiowrite('a.wav', output, fs);
+power = abs(y).^2/n;    
+% generate the plot and observe the frequency spectrum
+% Q1: found in 100 and 6000
 plot(t,x);
 plot(f(1:floor(n/2)),power(1:floor(n/2)))
 xlabel('Frequency')
 ylabel('Power')
+% filter original signal and save it to a.wav
+[b,a] = butter(4, [150,700]/(fs/2));
+output = filter(b,a,x);
+% Q2
+audiowrite('a.wav', output, fs);
